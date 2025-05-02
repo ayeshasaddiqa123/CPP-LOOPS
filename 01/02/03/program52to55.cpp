82082c859442154cd086c92ecdb158db0939e669
ayeshasaddiqa123 <ayeshasaddiqawebdev@gmail.com>
2025-05-02 22:47:42 +0500

    program52to55.cpp

--- 01/02/03/program52to55.cpp
+++ 01/02/03/program52to55.cpp
@@ -65,12 +65,32 @@ using namespace std ;   //use standard namespace
 int main()             // built-in function (main) from where execution starts
 
 {
-    // outer loop for rows
-	for(int i=1;i<=4;i++) {
-		cout<<i;
-		for(int j=2;j<=6;j+=2) //inner loop for columns
-			cout<<" "<<j;
-		cout<<endl; // print new line after each row
-	} // end of outer loop for rows
-return 0; //return 0 to  operating system
+    
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 5;
+    int cols = 11;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            // Determine positions of asterisks
+            if ((i == 0 && j == 5) ||             // Top of triangle
+                (i == 1 && (j == 4 || j == 6)) ||
+                (i == 2 && (j == 3 || j == 7)) ||
+                (i == 3 && (j == 2 || j == 8)) ||
+                (i == 4 && (j == 1 || j == 9)))   // Bottom row has 2 asterisks
+            {
+                cout << "* ";
+            }
+            else {
+                cout << "# ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;}
+
 }
